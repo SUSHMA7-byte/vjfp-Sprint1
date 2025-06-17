f3875786cc893eef569f361105b8a11b7b5b0473
Utkarsh Pandey <159171513+UtkarshPandey9@users.noreply.github.com>
2025-06-17 12:29:45 +0530

    Update DBUtil.java

--- util/DBUtil.java
+++ util/DBUtil.java
@@ -1,4 +1,15 @@
 package com.sprint1.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/job_fair_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
