0bae4288cc0016d51aa7cfa9ac03eb3fdd9ad8f9
Udayan Kashyap <kashyapudayan@gmail.com>
2025-06-18 10:29:13 +0530

    fix exceptions

--- src/com/sprint1/exception/applications/CandidateNotEligibleException.java
+++
@@ -1,7 +0,0 @@
-package com.sprint1.exception.applications;
-
-public class CandidateNotEligibleException extends Exception {
-  public CandidateNotEligibleException(String reason) {
-    super("Candidate not eligible: " + reason);
-  }
-}
--- src/com/sprint1/exception/employee/InvalidEmployeeRoleException.java
+++
@@ -1,7 +0,0 @@
-package com.sprint1.exception.employee;
-
-public class InvalidEmployeeRoleException extends Exception {
-  public InvalidEmployeeRoleException(String role) {
-    super("Invalid employee role: " + role);
-  }
-}
